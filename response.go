09ba8ca61fbb81b9afae979c63568c184c7a6307
E99p1ant <i@github.red>
2021-02-15 15:02:00 +0800

    response: adapt status code

--- response.go
+++ response.go
@@ -1,9 +1,22 @@
 package gadget
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // MakeErrJSON makes the error response JSON for gin.
 func MakeErrJSON(errCode int, msg string, a ...interface{}) (int, interface{}) {
+	if errCode < 100 {
+		errCode = 50000
+	}
+	// [100 - 999]
+	if errCode < 1000 {
+		errCode *= 100
+	}
+	// [1000 - 9999] or [10000 - ]
+	if errCode < 10000 || errCode > 99999 {
+		errCode = 50000
+	}
 	return errCode / 100, map[string]interface{}{"error": errCode, "msg": fmt.Sprintf(msg, a...)}
 }
 
